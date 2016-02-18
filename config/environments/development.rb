bf52c9fbdd9ae2b0c4384e15c88155eb00ec9faf
Azan Abrar <azan.abrar@gmail.com>
2016-02-18 10:12:47 +0500

    Update credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['eritheia.labs'],
-    :password       => ENV['labseritheia123'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'localhost:3000',
     :enable_starttls_auto => true
   }
